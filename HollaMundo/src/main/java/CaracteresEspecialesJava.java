ce73748fb08525e8724b778bf68a5ed4edf96389
Hatsembiller Octavio Daniel <hatsem78@gmail.com>
2021-06-16 12:27:43 -0300

    Clase Scanner en Java

---
+++ HollaMundo/src/main/java/CaracteresEspecialesJava.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author octavio
+ */
+public class CaracteresEspecialesJava {
+    public static void main(String args[]){
+    
+        String nombre = "Carla";
+        String apellido = "Perez";
+        
+        System.out.println(nombre + " " + apellido);
+        
+        System.out.println("Nueva linea \n " + nombre);
+        
+        System.out.println("Tabulador: \t" + apellido);
+        
+        System.out.println("Retroceder: \b" + apellido);
+        
+        System.out.println("Retorno de carro: \r" + apellido);
+        /*se utiliza para el retorno de carro independientemente del sistema operativo*/
+        System.lineSeparator();
+        System.out.println("hola");
+    
+    }
+    
+}
---
+++ HollaMundo/src/main/java/ClaseScannerJava.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author octavio
+ */
+public class ClaseScannerJava {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Ingresa el valor:");
+        var usuario = scanner.nextLine();
+        System.out.println("Entrada: " + usuario);
+        
+    }
+    
+}
