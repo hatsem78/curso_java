0ea3d6ac3ebbcc10a215dbfbbdd25957d1dbfac0
Hatsembiller Octavio Daniel <hatsem78@gmail.com>
2021-06-16 10:56:39 -0300

    Lección: Manejo de Variables en Java

---
+++ HollaMundo/src/main/java/HolaMundo.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author octavio
+ * pvsm es un atajo para crear el metodo main 
+ * sout atajo para crear System.out.print
+ */
+public class HolaMundo {
+    public static void main(String args[]){
+        System.out.println("Hola Mundo desde Java");
+        System.out.println("hola prueba de sout");
+        
+    }
+}
