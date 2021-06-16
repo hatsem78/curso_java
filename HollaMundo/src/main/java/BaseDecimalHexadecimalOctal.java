9eac5d82ba879c4c13d8006f1eefeb28b2b84be9
Hatsembiller Octavio Daniel <hatsem78@gmail.com>
2021-06-16 14:41:27 -0300

    Base Decimal, Hexadecimal y Octal

---
+++ HollaMundo/src/main/java/BaseDecimalHexadecimalOctal.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author octavio
+ */
+public class BaseDecimalHexadecimalOctal {
+    public static void main(String args[]){
+        int numeroDecimal = 10;
+        System.out.println("numeroDecimal = " + numeroDecimal);
+        
+        int numeroOctal = 012;
+        System.out.println("numeroOctal = " + numeroOctal);
+        
+        int numeroExadecimal = 0xA;
+        System.out.println("numeroExadecimal = " + numeroExadecimal);
+        
+        
+        int numeroBinario = 0b1010;
+        System.out.println("numeroBinario = " + numeroBinario);
+    }
+    
+}
